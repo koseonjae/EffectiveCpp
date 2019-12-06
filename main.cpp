90245bac32d7dc42f80d68606bfad4b6a9698822
koseonjae <goop702@gmail.com>
2019-12-07 00:13:46 +0900

    chanpter 13 자원 관리 객체인 shared_ptr을 사용

--- main.cpp
+++ main.cpp
@@ -5,31 +5,40 @@
 #include "DerivedAWOV.h"
 #include "DerivedNamedObject.h"
 
+void testChapter13( void )
+{
+    shared_ptr<DerivedNamedObject<int>> namedObject = make_shared<DerivedNamedObject<int>>( "four", 4, 4 );
+    shared_ptr<DerivedAWOV> derivedAWOV = make_shared<DerivedAWOV>();
+}
+
 int main()
 {
-    // CHAPTER 5
-    NamedObject object1( "one", 1 ); // constructor
-    NamedObject object2( object1 ); // copy constructor
-    object2 = object1; // copy assignment operator
+//    // CHAPTER 5
+//    NamedObject object1( "one", 1 ); // constructor
+//    NamedObject object2( object1 ); // copy constructor
+//    object2 = object1; // copy assignment operator
+//
+//    // CHAPTER 6
+//
+//    HomeForSale forSale1;
+//    HomeForSale forSale2;
+////    HomeForSale forSale3( forSale1 ); // delete 키워드로 복사 생성자를 막음
+////    forSale1 = forSale2; // delete 키워드로 인해 복사 대입 연산자를 막음
+//
+//    UncopyableHomeForSale uncopyableHomeForSale1;
+//    UncopyableHomeForSale uncopyableHomeForSale2;
+////    UncopyableHomeForSale uncopyableHomeForSale3( uncopyableHomeForSale1 ); // Uncopyable 상속으로 복사 생성자를 막음
+////    uncopyableHomeForSale1 = uncopyableHomeForSale2; // Uncopyable 상속으로 복사 대입 연산자를 막음
+//
+//    AWOV *awov = new DerivedAWOV();
+//    delete awov;
+//
+//    DerivedNamedObject derivedNamedObject1( "three", 3, 3 );
+//
+//    DerivedNamedObject derivedNamedObject2( derivedNamedObject1 );
 
-    // CHAPTER 6
-
-    HomeForSale forSale1;
-    HomeForSale forSale2;
-//    HomeForSale forSale3( forSale1 ); // delete 키워드로 복사 생성자를 막음
-//    forSale1 = forSale2; // delete 키워드로 인해 복사 대입 연산자를 막음
-
-    UncopyableHomeForSale uncopyableHomeForSale1;
-    UncopyableHomeForSale uncopyableHomeForSale2;
-//    UncopyableHomeForSale uncopyableHomeForSale3( uncopyableHomeForSale1 ); // Uncopyable 상속으로 복사 생성자를 막음
-//    uncopyableHomeForSale1 = uncopyableHomeForSale2; // Uncopyable 상속으로 복사 대입 연산자를 막음
-
-    AWOV *awov = new DerivedAWOV();
-    delete awov;
-
-    DerivedNamedObject derivedNamedObject1( "three", 3, 3 );
-
-    DerivedNamedObject derivedNamedObject2( derivedNamedObject1 );
+    // CHAPTER 13
+    testChapter13();
 
     return 0;
 }
