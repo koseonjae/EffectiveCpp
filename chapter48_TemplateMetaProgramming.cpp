8145ea22c916b0f8b96d2a30265f8029594865e9
koseonjae <goop702@gmail.com>
2020-01-31 03:34:24 +0900

    chapter 48, TMP를 통한 컴파일 타임 계산 + 코드량 증가?

---
+++ chapter48_TemplateMetaProgramming.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+template <unsigned n>
+struct Factorial
+{
+    enum
+    {
+        value = n * Factorial<n - 1>::value
+    };
+};
+
+template <>
+struct Factorial<0>
+{
+    enum
+    {
+        value = 1
+    };
+};
+
+int main( void )
+{
+    cout << Factorial<20>::value << endl;
+    return 0;
+}
